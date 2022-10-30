54f06c52ead2ac80d21ce3ebaf11c24e2e27e5a2
Lenovo <aunkyawin2035@gmail.com>
2022-10-30 10:45:55 +0630

    secondPrepare

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ export function showMyname(){
     return myname;
 };
 
-export function showMynameUi(selector){
-    document.querySelector(selector).innerHTML = myname;
-}
+export function showMynameUi(){
+    const h1 = document.createElement("h1")
+    h1.innerText = myname;
+    h1.style.color = "black"
+    h1.style.fontSize = "100px"
+    document.body.append(hq)
+};
