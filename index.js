bf25bdb039b286685cd8e06ff35afc749333e375
Lenovo <aunkyawin2035@gmail.com>
2022-10-30 10:55:29 +0630

    fourth

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ export function showMynameUi(){
     const h1 = document.createElement("h1")
     h1.innerText = myname;
     h1.style.color = "black"
-    h1.style.fontSize = "100px"
+    h1.style.fontSize =  100+"px";
+    h1.style.position = "fixed"
+    h1.style.top = 0;
     document.body.append(h1);
 };
